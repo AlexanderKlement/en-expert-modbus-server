a6dea37fd6a49a9c971a00eaa4158968a9589286
Alexander Klement <alexander.klement@gmail.com>
2023-06-16 10:53:34 +0200

    fixed the services, paths were wrong

--- services/en-expert-modbus-server.sh
+++ services/en-expert-modbus-server.sh
@@ -4,16 +4,16 @@
 case "$1" in
 start)
   echo "Starting en-expert modbus server service"
-  /usr/local/bin/python3 /opt/en-expert/modbus-server/server/main.py &
+  /usr/local/bin/python3 /opt/en-expert/modbus-server/main.py &
   ;;
 stop)
   echo "Stopping en-expert modbus server service"
-  pkill -f /opt/en-expert/modbus-server/server/main.py
+  pkill -f /opt/en-expert/modbus-server/main.py
   ;;
 restart)
   echo "Restarting en-expert modbus server service"
-  pkill -f /opt/en-expert/modbus-server/server/main.py
-  /usr/local/bin/python3 /opt/en-expert/modbus-server/server/main.py &
+  pkill -f /opt/en-expert/modbus-server/main.py
+  /usr/local/bin/python3 /opt/en-expert/modbus-server/main.py &
   ;;
 *)
   echo "Usage: /etc/init.d/en-expert-modbus-server {start|stop|restart}"

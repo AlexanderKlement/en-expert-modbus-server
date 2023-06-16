d2f1b0a946a1b3f05e384cee5658b186b2e4a86b
Alexander Klement <alexander.klement@gmail.com>
2023-06-16 12:07:04 +0200

    rewrote the server creation, because it seemed a bit off

--- server/main.py
+++ server/main.py
@@ -1,22 +1,32 @@
 import logging
 from pymodbus.datastore import ModbusSequentialDataBlock
 from pymodbus.datastore import ModbusSlaveContext, ModbusServerContext
+from pymodbus.device import ModbusDeviceIdentification
 from pymodbus.server.sync import StartTcpServer
 
 logging.basicConfig(filename='/var/log/en-expert-modbus-server.log', level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s')
 
 DEFAULT_VALUE = 0
-START_ADDRESS = 0
+START_ADDRESS = 0x0
 AMOUNT_REGISTERS = 0x2000
 
 
 def start_server():
-    store = ModbusSlaveContext(
-        hr=ModbusSequentialDataBlock(START_ADDRESS, [DEFAULT_VALUE] * AMOUNT_REGISTERS))
+    block = ModbusSequentialDataBlock(START_ADDRESS, [DEFAULT_VALUE] * AMOUNT_REGISTERS)
+    store = ModbusSlaveContext(hr=block)
     context = ModbusServerContext(slaves=store, single=True)
 
-    StartTcpServer(context)
+    identity = ModbusDeviceIdentification()
+    identity.VendorName = 'Pymodbus'
+    identity.ProductCode = 'PM'
+    identity.VendorUrl = 'http://github.com/riptideio/pymodbus/'
+    identity.ProductName = 'Pymodbus Server'
+    identity.ModelName = 'Pymodbus Server'
+    identity.MajorMinorRevision = '1.0'
+
+    logging.info("Starting Modbus Server/Slave...")
+    StartTcpServer(context, identity=identity)
 
 
 if __name__ == "__main__":

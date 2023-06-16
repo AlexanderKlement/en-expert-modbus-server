2dffb60a4f99bed4f64cca58b601fb52d05438e9
Alexander Klement <alexander.klement@gmail.com>
2023-06-16 11:25:58 +0200

    added new

--- updater/main.py
+++ updater/main.py
@@ -6,12 +6,14 @@ from pymodbus.constants import Endian
 from pymodbus.payload import BinaryPayloadBuilder
 from pymodbus.client.sync import ModbusTcpClient as ModbusClient
 
-
 logging.basicConfig(filename='/var/log/en-expert-modbus-updater.log', level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s')
 
 INTERVAL_SECONDS = 120  # 2 mins for start
-REGISTER_OFFSET = 8  # we use 2 * 32 bit, so we have another 2 available
+REGISTER_SIZE = 2  # 32 bit
+BLOCK_SIZE = 10
+REGISTER_OFFSET = REGISTER_SIZE * BLOCK_SIZE  # we use 2 * 32 bit, so we have another 2 available
+START = 0x1000
 HOST = "localhost"
 
 
@@ -33,14 +35,19 @@ def get_data() -> List[Tuple]:
 
 
 def generate_stub_data() -> List[Tuple]:
-    length = 20
+    length = 4
 
     stub_list = []
 
     for _ in range(length):
-        stub_list.append((random.uniform(0.0, 1000.0), random.uniform(0.0, 1000.0)))
+        stub_list.append(tuple(random.uniform(0.0, 1000.0) for _ in range(6)))
 
     return stub_list
+
+
+def set_fixed_data(client: ModbusClient):
+    write_float_to_modbus(client=client, starting_address=0x100, value=1)
+    write_float_to_modbus(client=client, starting_address=0x102, value=100)
 
 
 def update_modbus(data: List[Tuple]) -> None:
@@ -48,11 +55,12 @@ def update_modbus(data: List[Tuple]) -> None:
     try:
         if not client.connect():
             raise ConnectionError("Cannot connect to modbus server on {}".format(HOST))
+        set_fixed_data(client=client)
         for index, single_tuple in enumerate(data):
-            address = index * REGISTER_OFFSET
+            address = START + (index * REGISTER_OFFSET)
             for value in single_tuple:
                 write_float_to_modbus(client=client, starting_address=address, value=value)
-                address += 2  # 32 bit float uses 2 16-bit registers
+                address += REGISTER_SIZE  # 32 bit float uses 2 16-bit registers
     except Exception as err:
         logging.error("Cannot upload data to modbus server: %s", err)
     finally:
